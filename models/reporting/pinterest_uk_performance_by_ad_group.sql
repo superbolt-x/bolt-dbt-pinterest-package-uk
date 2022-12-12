183dac9611f7f32694c5b58483e0247530a408c1
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2022-12-12 15:22:12 +0100

    Update and rename pinterest_performance_by_ad_group.sql to pinterest_uk_performance_by_ad_group.sql

--- models/reporting/pinterest_uk_performance_by_ad_group.sql
+++ models/reporting/pinterest_uk_performance_by_ad_group.sql
@@ -1,11 +1,11 @@
 {{ config (
-    alias = target.database + '_pinterest_performance_by_ad_group'
+    alias = target.database + '_pinterest_uk_performance_by_ad_group'
 )}}
 
 {%- set date_granularity_list = ['day','week','month','quarter','year'] -%}
 {%- set exclude_fields = ['date','last_updated','unique_key'] -%}
 {%- set dimensions = ['advertiser_id','campaign_id','ad_group_id'] -%}
-{%- set measures = adapter.get_columns_in_relation(ref('pinterest_ad_groups_insights'))
+{%- set measures = adapter.get_columns_in_relation(ref('pinterest_uk_ad_groups_insights'))
                     |map(attribute="name")
                     |reject("in",exclude_fields)
                     |reject("in",dimensions)
@@ -26,23 +26,23 @@ WITH
         COALESCE(SUM({{ measure }}),0) as {{ measure }}
         {%- if not loop.last %},{%- endif %}
         {% endfor %}
-    FROM {{ ref('pinterest_ad_groups_insights') }}
+    FROM {{ ref('pinterest_uk_ad_groups_insights') }}
     GROUP BY {{ range(1, dimensions|length +2 +1)|list|join(',') }}),
     {%- endfor %}
 
     adgroups AS 
     (SELECT ad_group_id, ad_group_name, ad_group_status
-    FROM {{ ref('pinterest_ad_groups') }}
+    FROM {{ ref('pinterest_uk_ad_groups') }}
     ),
 
     campaigns AS 
     (SELECT campaign_id, campaign_name, campaign_status
-    FROM {{ ref('pinterest_campaigns') }}
+    FROM {{ ref('pinterest_uk_campaigns') }}
     ),
 
     advertisers AS 
     (SELECT advertiser_id, advertiser_name
-    FROM {{ ref('pinterest_advertisers') }}
+    FROM {{ ref('pinterest_uk_advertisers') }}
     )
 
 
